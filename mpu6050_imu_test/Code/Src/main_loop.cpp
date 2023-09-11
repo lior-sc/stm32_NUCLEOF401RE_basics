6185b25bcf6223664cd2a40ee6c027293dc5e2bd
Soltrex-io <lior@soltrex.io>
2023-09-11 11:24:24 +0300

    mpu6050_imu_test | add test routines for code
    NOTES
    * several loop routines were added to test library functions

--- mpu6050_imu_test/Code/Src/main_loop.cpp
+++ mpu6050_imu_test/Code/Src/main_loop.cpp
@@ -12,19 +12,41 @@ extern I2C_HandleTypeDef hi2c1;
 
 mpu6050 acc(GPIO_PIN_RESET,&hi2c1);
 
+// function decleration
+void routine_n1();
+void routine_n2();
+void routine_n3();
+
 void main_loop()
 {
 	while(1)
 	{
-		acc.init();
-		uint8_t imu_address = 0x00;
-		acc.who_am_i(&imu_address);
-		acc.set_sleep_enabled();
-		acc.set_sleep_disabled();
-		HAL_Delay(1000);
-		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		routine_n3();
 	}
 }
 
+void routine_n1()
+{
+//	acc.init();
+	uint8_t imu_address = 0x00;
+	acc.who_am_i(&imu_address);
+	acc.set_sleep_enabled();
+	acc.set_sleep_disabled();
+	HAL_Delay(1000);
+	HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+}
 
+void routine_n2()
+{
+	int16_t data[7] = {0};
+	acc.get_data_raw(data);
+	HAL_Delay(100);
+	return;
+}
 
+void routine_n3()
+{
+	acc.get_data();
+	HAL_Delay(100);
+	return;
+}
